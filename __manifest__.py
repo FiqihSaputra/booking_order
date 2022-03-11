579eadc8934cfdccd2d06d2603c2798bc40c7339
fiqih030501 <fiqih.hashmicro@gmail.com>
2022-03-11 17:42:15 +0700

    Commit kedua

--- __manifest__.py
+++ __manifest__.py
@@ -16,11 +16,12 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Administration',
     'version': '0.1',
+    'application':True,
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'sale', 'sale_management'],
 
     # always loaded
     'data': [
@@ -28,6 +29,8 @@
         'views/views.xml',
         'views/templates.xml',
         'views/menu_views.xml',
+        'views/service_team.xml',
+
 
     ],
     # only loaded in demonstration mode
